a5f7d2826672d467899213d925755bd3cb13ace2
MinesJA <minesja@gmail.com>
2021-01-23 19:16:44 -0500

    Added strong params

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -23,32 +23,52 @@ class GameController < ApplicationController
     
     deck = Deck.new()
   
-    holes = params[:holes].map{|player, card_shorts| 
+    holes = game_params[:holes].map{|player, card_shorts| 
       cards = card_shorts.map{|s| deck.pull(Card.from_str(short: s))}
       [player, cards]}.to_h
 
-    community =params[:community].map{|type, card_shorts| 
+    community = game_params[:community].map{|type, card_shorts| 
       cards = card_shorts.map{|s| deck.pull(Card.from_str(short: s))}
       [type, cards]}.to_h
                   
 
-    player_count = params[:player_count]
+    player_count = game_params[:player_count]
 
     rounds = play_rounds(
-      round_count: params[:round_count], 
-      deck: params[:rounds], 
+      round_count: game_params[:round_count], 
+      deck: game_params[:rounds], 
       holes: holes, 
       community: community,
     )
 
-    
-    
-    
+    render json: @tool, status: 200
 
-
-
-
- 
 
   end
+
+  private
+
+    # header = {
+  #   round_count: 100,
+  #   holes => {1 => ["KD", "3D"], 2 => [], 3 => [], 4 => []},
+  #   community => {
+  #     flop => ["4D", "5C", "JS"],
+  #     turn => [],
+  #     river => []
+  #   }
+  # }
+
+  def game_params
+    params.require(:game).permit(:round_count, :holes => {}, community: [flop: [], turn: [], river: []])
+  end
+
+  # header = {
+  #   round_count: 100,
+  #   holes => {1 => ["KD", "3D"], 2 => [], 3 => [], 4 => []},
+  #   community => {
+  #     flop => ["4D", "5C", "JS"],
+  #     turn => [],
+  #     river => []
+  #   }
+  # }
 end
